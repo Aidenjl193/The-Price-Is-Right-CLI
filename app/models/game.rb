29ec8a55b9abd57f597277acbc86095d90f05f77
aidenjl193 <aidenjl193@gmail.com>
2019-05-03 16:18:39 +0100

    Fleshed out functions and relationships

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,13 @@
 class Game < ActiveRecord::Base
+  belongs_to :user
+  has_many :answers
+
+  def answers
+    Answer.select{|answer| answer.game_id == id}
+  end
+
+  def get_question
+
+  end
 end
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
 class User < ActiveRecord::Base
+  has_many :games
+
+  def self.get_user_by_name(name)
+    all.find{|user| user.name == name}
+  end
+
+  def new_game
+    Game.new(user_id: id)
+  end
+
+  def games
+    Game.all.select{|game| game.user_id == id}
+  end
+
+  def accuracy
+    (games.map{|game| game.score / game.answers.length}.sum / games.length) * 100
+  end
+
+  def best_game
+     games.max_by{|game| game.score}
+  end
+  
+  def high_score
+    best_game.score
+  end
 end
     
