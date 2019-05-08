bc6d97d27b2e697c88ad47422864c5b6c02333c5
AmariEllington <amari.l.ellington@gmail.com>
2019-05-08 13:56:15 +0100

    start_menu added

--- run.rb
+++ run.rb
@@ -3,7 +3,7 @@ require 'sinatra/activerecord/rake'
 require 'tty-prompt'
 require 'artii'
 require 'colorize'
-
+require 'pry'
 
 
 def difficulties
@@ -13,6 +13,7 @@ def difficulties
   "Easy" => 30
   }
 end
+
 
 def brk
     puts "
@@ -48,21 +49,25 @@ def start_menu
   menu = TTY::Prompt.new
   selection = menu.select("") do |a|
     a.choice 'Play'
-    a.choice 'Instructions'
+    a.choice 'Debug'
     a.choice 'Exit game'
   end
 
   case selection
   when 'Play'
-        category
-  when 'Instructions'
-    instructions
+        header
+  when 'Debug'
+    binding.pry
+    "   "
+    start_menu
   when 'Exit game'
     a = Artii::Base.new :font => 'slant'  
     puts a.asciify('Goodbye!')
   end
 
 end
+
+
 
 def difficulty
   prompt = TTY::Prompt.new
@@ -85,7 +90,7 @@ def loading
     sleep(0.1)
   end
   
-  spinner.success('🍾🍾🍾🍾(successful)🍾🍾🍾🍾')
+  spinner.success('🍾🍾🍾🍾Successful🍾🍾🍾🍾')
 end
 
 def question_header
@@ -98,7 +103,7 @@ def get_user(name)
   if !user
     user = User.create(name: name)
   else
-    puts "👋👋👋👋Welcome back #{name}👋👋👋👋"
+    puts "👋👋👋👋 Welcome back #{name} 👋👋👋"
   end
   user
 end
@@ -107,10 +112,32 @@ def diff(a,b)
   (a - b).abs
 end
 
+def rank
+  a = Artii::Base.new :font => 'slant'
+  puts a.asciify("HIGH SCORES")
+table_data = [
+  { :name => User.scoreboard[0][:name], :score => User.scoreboard[0][:high_score]},
+  { :name => User.scoreboard[1][:name], :score => User.scoreboard[1][:high_score]},
+  { :name => User.scoreboard[2][:name], :score => User.scoreboard[2][:high_score]},
+  { :name => User.scoreboard[3][:name], :score => User.scoreboard[3][:high_score]},
+  { :name => User.scoreboard[4][:name], :score => User.scoreboard[4][:high_score]}
+]
+Formatador.display_table(table_data)
+end
 
+def user_stats(user)
+  a = Artii::Base.new :font => 'slant'
+  puts a.asciify("YOUR STATS")
+  table_data = [{
+     :games_played => user.stats[:high_score], 
+     :accuracy => user.stats[:accuracy]
+    }]
+  Formatador.display_table(table_data)
+end
 
 
 def run
+  start_menu
   name = header
   user = get_user(name)
   brk
@@ -130,18 +157,19 @@ def run
     question.guess = input
     question.save
     if diff(input.to_i, question.price) < diff_range
-      puts  "Correct, the price was #{question.price.to_i}👍😁".colorize(:green)
+      puts  "Correct, the price was #{question.price.to_i}👍 😁".colorize(:green)
       game.score += 1
       game.save
     else 
-      puts "Incorrect, the price was #{question.price.to_i}👎😭".colorize(:red)
+      puts "Incorrect, the price was #{question.price.to_i}👎 😭".colorize(:red)
     end
     question = game.get_question
   end
   a = Artii::Base.new :font => 'slant'
 puts a.asciify("You scored #{game.score} out of 10")
+rank
+user_stats(user)
   goodbye
 end
 
 run
-
